ebd7f330673177283cabe5515f959a4b0b8dfc86
Jack Aakash <jackaakash4@gmail.com>
2025-04-02 23:10:14 +0545

    Created: Postgres database is connected

---
+++ Backend/models/task.go
@@ -0,0 +1,11 @@
+package models
+
+import "gorm.io/gorm"
+
+type Task struct{
+    gorm.Model
+    Title       string `json:"title"`
+    Description string `json:"description"`
+    Status      string `json:"status"`
+    UserId      uint `json:"user_id"`
+}
